021b9c7042dbed15a8768397cbe735231104c680
ianmcnicholas <ianjmcnicholas@.com>
2023-12-08 10:55:50 +0000

    more on functions

---
+++ src/Functions.java
@@ -0,0 +1,16 @@
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Functions {
+    public static void main(String[] args) {
+        // A Function can take an input and output the same or a different type
+        // Function<T, R>; R apply(T t)
+
+        Function<String, Integer> fn1 = s -> s.length();
+        System.out.println("Checking string length: " + fn1.apply("String to check"));
+
+        // Bi Function: R apply(T t, U, u);
+        BiFunction<String, String, Integer> biFn1 = (s1, s2) -> (s1.concat(s2)).length();
+        System.out.println(biFn1.apply("This", "That")); // 8
+    }
+}
