03c55e710a05195b568b1501b40368fe3952c785
ianmcnicholas <ianjmcnicholas@.com>
2023-12-14 14:56:56 +0000

    working on lambda assignment

--- src/LambdasAssignment.java
+++ src/LambdasAssignment.java
@@ -1,17 +1,75 @@
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class LambdasAssignment {
     public static void main(String[] args) {
-        consumer("This is the string");
+//        consumer("This is the String");
+//        supplier();
+        predicate();
     }
+
 
     public static void consumer(String string) {
 //        Cannot find correct Printable interface so have made my own
         Printable<String> printing = s -> System.out.println(s);
         printing.print(string);
+        Consumer<String> consLb = s -> System.out.println(s);
+        Consumer<String> consMR = System.out::println;
+        consLb.accept("lambda String");
+        consMR.accept("MR String");
+    }
+    private static void supplier() {
+        Retrievable<Integer> retriever = () -> {return 77;};
+        retriever.get();
+        System.out.println(retriever.get());
 
+        Supplier<Integer> supLb = () -> {return 77;};
+        System.out.println(supLb.get() + " is the lambda");
+    }
+
+    private static void predicate(){
+        Evaluate<Integer> eval = i -> i < 0;
+        System.out.println(eval.test(-1));
+        System.out.println(eval.test(1));
+
+        Predicate<Integer> pred = i -> i < 0;
+        System.out.println(pred.test(-1));
+        System.out.println(pred.test(1));
+
+        System.out.println(check(4, i -> i % 2 == 0));
+        System.out.println(check(7, i -> i % 2 == 0));
+        System.out.println(check("Mr. Joe Bloggs", s -> s.startsWith("Mr.")));
+        System.out.println(check("Mrs. Joanna Bloggs", s -> s.startsWith("Mr.")));
+
+        System.out.println(check(new Person("Ian", 34, 1.77), p -> p.age >= 18));
+        System.out.println(check(new Person("Rosa", 7, 0.95), p -> p.age >= 18));
+    }
+
+    private static <T> boolean check(T t, Predicate<T> function){
+        return function.test(t);
     }
 }
 
-interface Printable<String> {
-    void print(String string);
+interface Printable<T> {
+    void print(T t);
+}
+
+interface Retrievable<T> {
+    T get();
+}
+interface Evaluate<T>{
+    boolean test(T t);
+}
+
+class Person {
+    String name;
+    int age;
+    double height;
+
+    public Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
 }
--- src/TestPredicate.java
+++ src/TestPredicate.java
@@ -1,15 +1,14 @@
-import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
-interface Evaluate<T> {
+interface Evaluating<T> {
     boolean isNegative(T t); // Type T refers to "any generic"
 }
 
 public class TestPredicate {
     public static void main(String[] args) {
 
-        Evaluate<Integer> lambda = i -> i < 0;
-        Evaluate<Integer> lambda2 = i -> { return i < 0;}; // this is the same as above
+        Evaluating<Integer> lambda = i -> i < 0;
+        Evaluating<Integer> lambda2 = i -> { return i < 0;}; // this is the same as above
         // in this case, i is in place of (), as the method isNegative takes one argument.
         // i < 0 is the implementation of the method itself.
         // Can use any generic, not just Integer.
